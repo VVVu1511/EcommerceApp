7a54b7a8bfbda43ce4392732d1220a976523d849
VVVu1511 <161138657+VVVu1511@users.noreply.github.com>
2025-03-31 22:07:39 +0700

    first commit

---
+++ Shop/src/main/java/model/Product.java
@@ -0,0 +1,39 @@
+package model;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String brand;
+	private BigDecimal price;
+	private int inventory;
+	private String description;
+	
+	private Category category;
+	
+	@OneToMany(mappedBy = "product" , cascade = CascadeType.ALL, orphanRemoval = true)
+	
+	private List<Image> images;
+}
