5786c17422fc1dcc9d14bd96da2a31b9c114f38f
VVVu1511 <161138657+VVVu1511@users.noreply.github.com>
2025-04-08 21:52:27 +0700

    create first API

--- Shop/src/main/java/com/Vu/Shop/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Vu.Shop.repository;
-
-import java.util.Locale.Category;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long>{
-	 Object findByName(String name);
-}
---
+++ hello/src/main/java/com/Vu/hello/controller/HelloSpringController.java
@@ -0,0 +1,14 @@
+package com.Vu.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloSpringController {
+	@GetMapping("/hello")
+	String sayHello() {
+		return "Hello time, again";
+	}
+	
+	
+}
