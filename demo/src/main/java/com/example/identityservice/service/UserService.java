ac4801edc6c523cf95816149506da3f975a30d40
VVVu1511 <161138657+VVVu1511@users.noreply.github.com>
2025-04-14 08:02:43 +0700

    authorize

--- demo/src/main/java/com/example/identityservice/service/UserService.java
+++ demo/src/main/java/com/example/identityservice/service/UserService.java
@@ -3,9 +3,8 @@ package com.example.identityservice.service;
 import java.util.HashSet;
 import java.util.List;
 
-import org.hibernate.transform.ToListResultTransformer;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.example.identityservice.dto.request.UserCreationRequest;
@@ -17,14 +16,15 @@ import com.example.identityservice.exception.AppException;
 import com.example.identityservice.exception.ErrorCode;
 import com.example.identityservice.mapper.UserMapper;
 import com.example.identityservice.repository.UserRepository;
-
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
 @FieldDefaults(level=AccessLevel.PRIVATE, makeFinal = true)
+@Slf4j
 public class UserService {
 	UserRepository userRepository;
 	UserMapper userMapper;
@@ -61,11 +61,17 @@ public class UserService {
 		userRepository.deleteById(userId);
 	}
 	
+	
+	@PreAuthorize("hasRole('ADMIN')")
 	public List<UserResponse> getUsers(){
+		log.info("In method get Users");
 		return userRepository.findAll().stream().map(userMapper::toUserResponse).toList();
 	}
 	
+	@PostAuthorize("hasRole('ADMIN')")
+	
 	public UserResponse getUser(String id) {
+		log.info("In method get user by Id");
 		return userMapper.toUserResponse(userRepository.findById(id)
 				.orElseThrow(() -> new RuntimeException("User not found!"))
 				);
