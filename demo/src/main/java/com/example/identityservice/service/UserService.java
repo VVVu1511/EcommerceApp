2005a5b0c33e16bb7528270f070f02c2d15119d4
VVVu1511 <161138657+VVVu1511@users.noreply.github.com>
2025-04-15 20:57:21 +0700

    add server

--- demo/src/main/java/com/example/identityservice/service/UserService.java
+++ demo/src/main/java/com/example/identityservice/service/UserService.java
@@ -2,9 +2,12 @@ package com.example.identityservice.service;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.example.identityservice.dto.request.UserCreationRequest;
@@ -57,10 +60,20 @@ public class UserService {
 		return userMapper.toUserResponse(user);
 	}
 	
+	public UserResponse getMyInfo() {
+		SecurityContext context = SecurityContextHolder.getContext();
+		
+		String name = context.getAuthentication().getName();
+		
+		User user = userRepository.findByUsername(name).orElseThrow(
+				() -> new AppException(ErrorCode.USER_NOT_EXIST));
+		
+		return userMapper.toUserResponse(user);
+	}
+	
 	public void deleteUser(String userId) {
 		userRepository.deleteById(userId);
 	}
-	
 	
 	@PreAuthorize("hasRole('ADMIN')")
 	public List<UserResponse> getUsers(){
@@ -68,8 +81,7 @@ public class UserService {
 		return userRepository.findAll().stream().map(userMapper::toUserResponse).toList();
 	}
 	
-	@PostAuthorize("hasRole('ADMIN')")
-	
+	@PostAuthorize("returnObject.username == authentication.name")	
 	public UserResponse getUser(String id) {
 		log.info("In method get user by Id");
 		return userMapper.toUserResponse(userRepository.findById(id)
