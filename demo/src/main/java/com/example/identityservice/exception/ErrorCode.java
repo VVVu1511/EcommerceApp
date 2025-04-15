96888c437c002457624adc3979463490cf7c3600
VVVu1511 <161138657+VVVu1511@users.noreply.github.com>
2025-04-15 21:06:26 +0700

    advanced exception

--- demo/src/main/java/com/example/identityservice/exception/ErrorCode.java
+++ demo/src/main/java/com/example/identityservice/exception/ErrorCode.java
@@ -1,20 +1,29 @@
 package com.example.identityservice.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+import lombok.Getter;
+
+
+@Getter
 public enum ErrorCode {
-	INVALID_KEY(1001, "Invalid message key!"),
-	UNCATEGORIZED_EXCEPTION(9999,"Uncategorized exception"),
-	USER_EXIST(1002,"User exists"),
-	USERNAME_INVALID(1003, "Username must be at least 3 characters"),
-	PASSWORD_INVALID(1004, "Password must be at least 8 characters"),
-	USER_NOT_EXIST(1005, "User not exist"),
-	UNAUTHENTICATED(1006, "Unauthenticated");
+	INVALID_KEY(1001, "Invalid message key!", HttpStatus.BAD_REQUEST),
+	UNCATEGORIZED_EXCEPTION(9999,"Uncategorized exception",HttpStatus.INTERNAL_SERVER_ERROR),
+	USER_EXIST(1002,"User exists", HttpStatus.BAD_REQUEST),
+	USERNAME_INVALID(1003, "Username must be at least 3 characters", HttpStatus.BAD_REQUEST),
+	PASSWORD_INVALID(1004, "Password must be at least 8 characters", HttpStatus.BAD_REQUEST),
+	USER_NOT_EXIST(1005, "User not exist", HttpStatus.BAD_REQUEST),
+	UNAUTHENTICATED(1006, "Unauthenticated", HttpStatus.BAD_REQUEST);
 	
 	private int code;
 	private String message;
+	private HttpStatusCode statusCode;
 	
-	private ErrorCode(int code, String message) {
+	private ErrorCode(int code, String message, HttpStatusCode statusCode) {
 		this.code = code;
 		this.message = message;
+		this.statusCode = statusCode;
 	}
 	public int getCode() {
 		return code;
@@ -22,4 +31,6 @@ public enum ErrorCode {
 	public String getMessage() {
 		return message;
 	}
+	
+	
 }
