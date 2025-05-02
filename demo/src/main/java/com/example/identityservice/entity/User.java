37895fe33a7ed422779ab006445367df31eafb6f
VVVu1511 <161138657+VVVu1511@users.noreply.github.com>
2025-05-02 16:13:11 +0700

    permission

--- demo/src/main/java/com/example/identityservice/entity/User.java
+++ demo/src/main/java/com/example/identityservice/entity/User.java
@@ -1,9 +1,7 @@
 package com.example.identityservice.entity;
 
-import java.lang.System.Logger.Level;
 import java.time.LocalDate;
 import java.util.Set;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,19 +10,20 @@ import jakarta.persistence.ManyToMany;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
+
+
+@NoArgsConstructor
+@Entity
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor
 @Getter
 @Setter
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
-@Entity
-@FieldDefaults(level = AccessLevel.PRIVATE)
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.UUID)
--- demo/src/main/java/com/example/identityservice/service/AuthenticationService.java
+++ demo/src/main/java/com/example/identityservice/service/AuthenticationService.java
@@ -37,12 +37,14 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.experimental.NonFinal;
+import lombok.extern.slf4j.Slf4j;
 
 
 
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class AuthenticationService {
 	UserRepository userRepository;
